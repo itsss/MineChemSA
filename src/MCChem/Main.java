1f45fe4c6138d66e7d193ea1afbdfdce1373cbfb
Taewon Kang <itschool@itsc.kr>
2018-01-12 14:41:49 +0900

    Make H2O2, H Boom! Effect

--- src/MCChem/Main.java
+++ src/MCChem/Main.java
@@ -1,8 +1,10 @@
 package MCChem;
 
+import net.minecraft.server.v1_10_R1.BlockGlass;
 import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.SplashPotion;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
@@ -11,9 +13,11 @@ import org.bukkit.inventory.CraftingInventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.material.Dye;
 import org.bukkit.material.Wool;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.Listener;
+import org.bukkit.potion.Potion;
 import org.bukkit.potion.PotionType;
 
 import java.util.logging.Logger;
@@ -21,7 +25,7 @@ import java.util.logging.Logger;
 public class Main extends JavaPlugin implements Listener
 {
     //private ShapedRecipe recipe;
-    private ShapedRecipe o2, candy, metmok, metcol, hcl;
+    private ShapedRecipe o2, candy, metmok, luck, hcl, egg, reg;
     Logger log;
     public void onEnable()
     {
@@ -39,7 +43,7 @@ public class Main extends JavaPlugin implements Listener
         hcl.setIngredient('C', new Wool(DyeColor.ORANGE));
         this.getServer().addRecipe(hcl);
 
-        candy = new ShapedRecipe(new ItemStack(Material.SUGAR, 1));
+        candy = new ShapedRecipe(new ItemStack(Material.SUGAR, 20));
         candy.shape("   ", "CHO", "   ");
         candy.setIngredient('C', new Wool(DyeColor.PINK));
         candy.setIngredient('H', new Wool(DyeColor.ORANGE));
@@ -52,6 +56,33 @@ public class Main extends JavaPlugin implements Listener
         metmok.setIngredient('C', new Wool(DyeColor.PINK));
         metmok.setIngredient('H', new Wool(DyeColor.ORANGE));
         this.getServer().addRecipe(metmok);
+
+        Potion potionluck = new Potion(PotionType.LUCK);
+        potionluck.setSplash(true);
+        ItemStack itemstack2 = potionluck.toItemStack(1);
+        luck = new ShapedRecipe(itemstack2);
+        luck.shape("   ", "HCO", "   ");
+        luck.setIngredient('H', new Wool(DyeColor.ORANGE));
+        luck.setIngredient('C', new Wool(DyeColor.PINK));
+        luck.setIngredient('O', new Wool(DyeColor.SILVER));
+        this.getServer().addRecipe(luck);
+
+        Potion potionreg = new Potion(PotionType.REGEN);
+        potionreg.setSplash(true);
+        ItemStack itemstack3 = potionreg.toItemStack(1);
+        reg = new ShapedRecipe(itemstack3);
+        reg.shape("   ", " HO", "   ");
+        reg.setIngredient('H', new Wool(DyeColor.ORANGE));
+        reg.setIngredient('O', new Wool(DyeColor.SILVER));
+        this.getServer().addRecipe(reg);
+
+        egg = new ShapedRecipe(new ItemStack(Material.EGG, 1));
+        egg.shape("   ", "ACO", "   ");
+        ItemStack glass = new ItemStack(Material.STAINED_GLASS, 1, (short)3);
+        egg.setIngredient('A', glass.getData());
+        egg.setIngredient('C', new Wool(DyeColor.PINK));
+        egg.setIngredient('O', new Wool(DyeColor.SILVER));
+        this.getServer().addRecipe(egg);
     }
 
     public void onDisable()
@@ -85,12 +116,76 @@ public class Main extends JavaPlugin implements Listener
     @EventHandler
     public void onPrepareItemCraft(PrepareItemCraftEvent event)
     {
-        PotionMeta meta = (PotionMeta) event.getRecipe().getResult().getItemMeta();
-        //log.info("meta" + meta.getBasePotionData().getType());
+        if(event.getRecipe().getResult().getType().equals(Material.SPLASH_POTION))
+        {
+            PotionMeta meta = (PotionMeta) event.getRecipe().getResult().getItemMeta();
+            /*if(meta.getBasePotionData().getType().equals(PotionType.INSTANT_HEAL))
+            {
+                log.info("HEAL POTION DETECTED");
+                CraftingInventory inventory = event.getInventory();
+                boolean f1 = false, f2 = false;
+                for(ItemStack item : inventory.getMatrix())
+                {
+                    if(item.getType().equals(Material.WOOL) && item.getDurability() == 1 && item.getAmount() >= 2)
+                    {
+                        f1 = true;
+                    }
+                    if(item.getType().equals(Material.WOOL) && item.getDurability() == 8 && item.getAmount() >= 2)
+                    {
+                        f2 = true;
+                    } //H2O2 => 투척치료 포션
+                }
+                if(!f1 || !f2) inventory.setResult(null);
+            }*/
+
+            if(meta.getBasePotionData().getType().equals(PotionType.LUCK))
+            {
+                log.info("luck detected");
+                CraftingInventory inventory = event.getInventory();
+                boolean f1 = false, f2 = false, f3 = false;
+                for(ItemStack item : inventory.getMatrix())
+                {
+                    if(item.getType().equals(Material.WOOL) && item.getDurability() == 6 && item.getAmount() >= 1)
+                    {
+                        f1 = true;
+                    }
+                    if(item.getType().equals(Material.WOOL) && item.getDurability() == 1 && item.getAmount() >= 2)
+                    {
+                        f2 = true;
+                    }
+                    if(item.getType().equals(Material.WOOL) && item.getDurability() == 8 && item.getAmount() >= 3)
+                    {
+                        f3 = true;
+                    } //H2CO3 => 행운 포션
+                }
+                if(!f1 || !f2 || !f3) inventory.setResult(null);
+            }
+            if(meta.getBasePotionData().getType().equals(PotionType.REGEN))
+            {
+                log.info("regen detected");
+                CraftingInventory inventory = event.getInventory();
+                boolean f1 = false, f2 = false;
+                for(ItemStack item : inventory.getMatrix())
+                {
+                    if(item.getType().equals(Material.WOOL) && item.getDurability() == 1 && item.getAmount() >= 2)
+                    {
+                        f1 = true;
+                    }
+                    if(item.getType().equals(Material.WOOL) && item.getDurability() == 8 && item.getAmount() >= 2)
+                    {
+                        f2 = true;
+                    }
+                }
+                if(!f1 || !f2) inventory.setResult(null);
+            }
+
+        }
+        /*PotionMeta meta = (PotionMeta) event.getRecipe().getResult().getItemMeta();
+        log.info("meta" + meta.getBasePotionData().getType());
         if(meta.getBasePotionData().getType().equals(PotionType.INSTANT_DAMAGE))
         {
             log.info("Instant Damage Potion Detected");
-        }
+        }*/
         if(event.getRecipe().getResult().getType().equals(Material.SUGAR)) //사탕수수 조합 제작
         { //PINK, ORANGE, SILVER
             log.info("candy getrecipe");
@@ -155,6 +250,35 @@ public class Main extends JavaPlugin implements Listener
                 if (!found1 || !found2) inventory.setResult(null);
             }
         }
+        if(event.getRecipe().getResult().getType().equals(Material.EGG))
+        {
+            log.info("egg getrecipe");
+            CraftingInventory inventory = event.getInventory();
+            boolean found1 = false, found2 = false, found3 = false;
+            for(ItemStack item : inventory.getMatrix())
+            {
+                ItemStack glass = new ItemStack(Material.STAINED_GLASS, 1, (short)3);
+                //egg.setIngredient('A', glass.getData());
+                if(item.getType().equals(Material.STAINED_GLASS) && item.getDurability() == 3)
+                {
+                    log.info("gla1");
+                    found1 = true;
+                }
+                //C = 6, O = 8
+                if(item.getType().equals(Material.WOOL) && item.getDurability() == 6)
+                {
+                    log.info("gla2");
+                    found2 = true;
+                }
+                if(item.getType().equals(Material.WOOL) && item.getDurability() == 8 && item.getAmount() >= 3)
+                {
+                    log.info("gla3");
+                    found3 = true;
+                }
+            }
+            if(!found1 || !found2 || !found3) inventory.setResult(null);
+        }
+        //CaCO3 달걀
     }
 
     @EventHandler
@@ -162,6 +286,95 @@ public class Main extends JavaPlugin implements Listener
     {
         log.info("onCraftItem Enabled");
         //log.info("test[onCraft] : " + );
+        if(event.getRecipe().getResult().getType().equals(Material.SPLASH_POTION))
+        {
+            PotionMeta meta = (PotionMeta) event.getRecipe().getResult().getItemMeta();
+            /*if(meta.getBasePotionData().getType().equals(PotionType.INSTANT_HEAL))
+            {
+                log.info("HEAL POTION DETECTED");
+                CraftingInventory inventory = event.getInventory();
+                boolean f1 = false, f2 = false;
+                ItemStack found1 = null, found2 = null;
+                for(ItemStack item : inventory.getMatrix())
+                {
+                    if(item.getType().equals(Material.WOOL) && item.getDurability() == 1 && item.getAmount() >= 2)
+                    {
+                        f1 = true;
+                        found1 = item;
+                    }
+                    if(item.getType().equals(Material.WOOL) && item.getDurability() == 8 && item.getAmount() >= 2)
+                    {
+                        f2 = true;
+                        found2 = item;
+                    } //H2O2 => 투척치료 포션
+                }
+                if(!f1 || !f2) inventory.setResult(null);
+                else
+                {
+                    found1.setAmount(found1.getAmount()-3);
+                    found2.setAmount(found1.getAmount()-3);
+                }
+            }*/
+            if(meta.getBasePotionData().getType().equals(PotionType.LUCK))
+            {
+                log.info("luck detected");
+                CraftingInventory inventory = event.getInventory();
+                boolean f1 = false, f2 = false, f3 = false;
+                ItemStack found1 = null, found2 = null, found3 = null;
+                for(ItemStack item : inventory.getMatrix())
+                {
+                    if(item.getType().equals(Material.WOOL) && item.getDurability() == 6 && item.getAmount() >= 1)
+                    {
+                        f1 = true;
+                        found1 = item;
+                    }
+                    if(item.getType().equals(Material.WOOL) && item.getDurability() == 1 && item.getAmount() >= 2)
+                    {
+                        f2 = true;
+                        found2 = item;
+                    }
+                    if(item.getType().equals(Material.WOOL) && item.getDurability() == 8 && item.getAmount() >= 3)
+                    {
+                        f3 = true;
+                        found3 = item;
+                    } //H2CO3 => 행운 포션
+                }
+                if(!f1 || !f2 || !f3) inventory.setResult(null);
+                else
+                {
+                    found1.setAmount(found1.getAmount()-2);
+                    found2.setAmount(found2.getAmount()-3);
+                    found3.setAmount(found3.getAmount()-4);
+                }
+            }
+
+            if(meta.getBasePotionData().getType().equals(PotionType.REGEN))
+            {
+                log.info("regen detected2");
+                CraftingInventory inventory = event.getInventory();
+                boolean f1 = false, f2 = false;
+                ItemStack found1 = null, found2 = null;
+                for(ItemStack item : inventory.getMatrix())
+                {
+                    if(item.getType().equals(Material.WOOL) && item.getDurability() == 1 && item.getAmount() >= 2)
+                    {
+                        f1 = true;
+                        found1 = item;
+                    }
+                    if(item.getType().equals(Material.WOOL) && item.getDurability() == 8 && item.getAmount() >= 2)
+                    {
+                        f2 = true;
+                        found2 = item;
+                    }
+                }
+                if(!f1 || !f2) inventory.setResult(null);
+                else
+                {
+                    found1.setAmount(found1.getAmount()-3);
+                    found2.setAmount(found2.getAmount()-3);
+                }
+            }
+        }
 
         if (event.getRecipe().getResult().getType().equals(Material.SUGAR)) //사탕수수 조합 제작
         { //PINK, ORANGE, SILVER
@@ -254,6 +467,43 @@ public class Main extends JavaPlugin implements Listener
                 }
             }
         }
+        if(event.getRecipe().getResult().getType().equals(Material.EGG))
+        {
+            log.info("egg getrecipe");
+            CraftingInventory inventory = event.getInventory();
+            boolean found1 = false, found2 = false, found3 = false;
+            ItemStack f1 = null, f2 = null, f3 = null;
+            for(ItemStack item : inventory.getMatrix())
+            {
+                //egg.setIngredient('A', glass.getData());
+                if(item.getType().equals(Material.STAINED_GLASS) && item.getDurability() == 3)
+                {
+                    log.info("gla1");
+                    f1 = item;
+                    found1 = true;
+                }
+                //C = 6, O = 8
+                if(item.getType().equals(Material.WOOL) && item.getDurability() == 6)
+                {
+                    log.info("gla2");
+                    f2 = item;
+                    found2 = true;
+                }
+                if(item.getType().equals(Material.WOOL) && item.getDurability() == 8 && item.getAmount() >= 3)
+                {
+                    log.info("gla3");
+                    f3 = item;
+                    found3 = true;
+                }
+            }
+            if(!found1 || !found2 || !found3) inventory.setResult(null);
+            else
+            {
+                f1.setAmount(f1.getAmount()-1);
+                f2.setAmount(f2.getAmount()-1);
+                f3.setAmount(f3.getAmount()-4);
+            }
+        }
     }
 
     @EventHandler
@@ -303,6 +553,29 @@ public class Main extends JavaPlugin implements Listener
                 b.setData(CropState.RIPE.getData());
             }
 
+        }
+
+        else if (e.getBlockPlaced().getType().equals(Material.FIRE)) //H Boom
+        {
+            log.info("fire install");
+            Player player = e.getPlayer();
+            World world = player.getWorld();
+            double x1 = e.getBlockPlaced().getLocation().getBlockX();
+            double y1 = e.getBlockPlaced().getLocation().getBlockY();
+            double z1 = e.getBlockPlaced().getLocation().getBlockZ();
+
+            Block b1 = new Location(world, x1, y1-1, z1).getBlock();
+            if(b1.getType() == Material.WOOL)
+            {
+                Wool wool = new Wool(b1.getType(), b1.getData());
+                DyeColor color = wool.getColor();
+                if(color.equals(DyeColor.ORANGE))
+                {
+                    log.info("fire BOOM!!!");
+                    player.getWorld().createExplosion(b1.getLocation(), 15F); //규모 15F (크리퍼 4F)
+                    player.getWorld().playEffect(b1.getLocation(), Effect.MOBSPAWNER_FLAMES, 10);
+                }
+            }
         }
     }
 }
